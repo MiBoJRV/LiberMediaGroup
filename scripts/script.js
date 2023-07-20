c33a7645b40352c1803a457521ae2a682cb12f29
Mykhailo Bodnar <mykhailo.bodnar@wetelo.com>
2023-07-20 09:48:03 +0300

    add structure

--- scripts/script.js
+++ scripts/script.js
@@ -19,11 +19,11 @@ function sendLeadData(event) {
   };
 
   // Proxy server URL
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   // Target API URL
   const apiUrl = 'https://tracker.pablo.partners/repost.php?act=register';
 
-  fetch(`${proxyUrl}${apiUrl}`, {
+  fetch(`${apiUrl}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
